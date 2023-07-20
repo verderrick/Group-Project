152c2019638e863ba3b80de3a083c2acd3c07565
verderrick <verderrick@gmail.com>
2023-07-20 23:38:37 +0800

    added code to overheads.py

---
+++ overheads.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import csv
+
+# create a file to csv file.
+fp = Path.cwd()/"CSV Reports"/"overheads.csv"
+
+# read the csv file to append profit and quantity from the csv.
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader) # skip header
+
+    # create an empty lists to store time sheet and sales record
+    overheads = [] 
+
+    # append time sheet and sales record into the salesRecords list
+    for row in reader:
+        #get the employee id, toal hours, break hours, and sales for each record
+        #and append the salesRecords list
+        overheads.append([row[0],float(row[1])])   
+
+higest_overheads = []
+for item in overheads:
+    higest_overheads.append(item[1])
+
+for item in overheads:
+    if item[1] == max(higest_overheads):
+        print(item)
+
+
